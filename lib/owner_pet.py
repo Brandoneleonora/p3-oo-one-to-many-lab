4163774df54576a5de4cf5cee7c116c7e6f82a7d
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-08-17 23:00:27 -0400

    complete project

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type in self.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception("You need an animal type inside on of our pet types")
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+
+    
+
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in  Pet.all if pet.owner == self]
+
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Pet must be an instance of Pet Class")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        names = [pet for pet in  Pet.all if pet.owner == self]
+        names.sort(key= lambda obj : obj.name)
+        return names
